120193e96b9252335fb1a4f81ab876454970de68
Kuukisann <kuukisann@hotmail.com>
2018-04-20 17:43:15 +0800

    热点推送->新闻公告

--- components/layout/header.vue
+++ components/layout/header.vue
@@ -4,7 +4,7 @@
     <div flex="cross:center">
       <el-menu router default-active="/" active-text-color="#409EFF" class="menu" mode="horizontal">
         <el-menu-item class="item" index="/">&#12288;首页&#12288;</el-menu-item>
-        <el-menu-item class="item" index="/msg">热点推送</el-menu-item>
+        <el-menu-item class="item" index="/msg">新闻公告</el-menu-item>
         <el-menu-item class="item" index="/policy">政策法规</el-menu-item>
       </el-menu>
       <div>
