1b77f82aa5e1975d73948410fc9f6767ff0b4ab4
王帅琪 <961345333@qq.com>
2018-04-21 17:20:04 +0800

    merge

--- components/home/researchList.vue
+++ components/home/researchList.vue
@@ -1,30 +1,33 @@
 <template>
   <div>
-    <div v-for="research in list" v-bind:key="research.id">
-      <el-button type="text" style="padding:0">{{research.name}}</el-button>
-      <div flex="main:justify cross:center">
-        <span>
-          <span>科研成果类型：</span>
-          <span>{{research.type}}</span>
-        </span>
-        <span>
-          <span>来自：</span>
-          <el-button>
-            {{research.lab}}
-          </el-button>
-        </span>
-
-      </div>
+    <div v-for="research in list" flex="dir:top" v-bind:key="research.id" class="list-item">
+      <span>
+        <div flex="main:justify">
+          <el-button type="text" style="padding:0">{{research.name}}</el-button>
+          <span style="color:#bcbcbc">
+            来自:
+            <el-button type="text">
+              {{research.lab}}
+            </el-button>
+          </span>
+        </div>
+        <span>科研成果类型：</span>
+        <span>{{research.type}}</span>
+      </span>
     </div>
   </div>
 </template>
 <style scoped>
-li {
-  padding: 10px;
-  border-top: 1px solid #228eff;
+.list-item {
+  border-bottom: 1px solid #bcbcbc;
+  padding: 1em;
 }
-li:first-child {
-  border-top: 0;
+.list-item:last-child {
+  border-bottom: 0;
+  padding-bottom: 0;
+}
+.list-item:first-child {
+  padding-top: 0;
 }
 </style>
 <script>
