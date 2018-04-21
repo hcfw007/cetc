b9ff5179313b6c33dbe81ed378508452c1089b59
Kuukisann <kuukisann@hotmail.com>
2018-04-21 20:56:37 +0800

    Login dialog

---
+++ components/user/loginForm.vue
@@ -0,0 +1,32 @@
+<template>
+  <el-form :model="form">
+    <el-form-item>
+      <el-input v-model="form.username" placeholder="用户名"> </el-input>
+    </el-form-item>
+    <el-form-item>
+      <el-input type="password" v-model="form.password" placeholder="密码"> </el-input>
+    </el-form-item>
+    <el-button type="primary" @click="onSubmit">登陆</el-button>
+  </el-form>
+</template>
+<style scoped>
+
+</style>
+<script>
+export default {
+  name: "login-form",
+  data() {
+    return {
+      form: {
+        username: "",
+        password: ""
+      }
+    };
+  },
+  methods: {
+    onSubmit() {
+      return false;
+    }
+  }
+};
+</script>
---
+++ components/user/userDialog.vue
@@ -0,0 +1,28 @@
+<template>
+  <div >
+    <el-radio-group v-model="formType" style="margin-bottom:22px">
+      <el-radio-button label="登陆"></el-radio-button>
+      <el-radio-button label="注册"></el-radio-button>
+    </el-radio-group>
+    <login-form v-if="formType==='登陆'"> </login-form>
+    <signup-form v-if="formType==='注册'"> </signup-form>
+  </div>
+</template>
+<style scoped>
+
+</style>
+
+<script>
+import loginForm from "~/components/user/loginForm";
+import signupForm from "~/components/user/signupForm";
+export default {
+  name: "user-dialog",
+  components: { loginForm, signupForm },
+  data() {
+    return {
+      formType: "登陆"
+    };
+  }
+};
+</script>
+
